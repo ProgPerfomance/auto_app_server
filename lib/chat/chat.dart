80c1316d579ce647bea6fc364d0557599379adc1
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-03-05 16:44:39 +0300

    ...

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -31,7 +31,7 @@ print('uid $uid');
     var data = row.assoc();
     print(data);
     final user = await sql.execute(
-      "SELECT * FROM users where id = ${data['uid1'] != uid ? data['uid1'] : data['uid2']}",
+      "SELECT * FROM users where id = ${data['uid1']}",
       {},
     );
     chats.add(
