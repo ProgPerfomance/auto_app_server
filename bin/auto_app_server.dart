c128cc2a9ffba1947099e205ae36d6cd33919aa9
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-03-05 11:54:01 +0300

    ...

--- bin/auto_app_server.dart
+++ bin/auto_app_server.dart
@@ -8,6 +8,7 @@ import 'package:auto_app_server/create_user_car_from_sql.dart';
 import 'package:auto_app_server/create_user_from_sql.dart';
 import 'package:auto_app_server/get_booking_list_from_sql.dart';
 import 'package:auto_app_server/get_car_list.dart';
+import 'package:auto_app_server/get_garages.dart';
 import 'package:auto_app_server/get_user_cars_form_sql.dart';
 import 'package:auto_app_server/like_car_from_sql.dart';
 import 'package:auto_app_server/sell_car/sell_car_list.dart';
@@ -228,6 +229,12 @@ void main() async {
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     final response = await getUserChats(uid: data['uid'], sql: sql);
+    return Response.ok(jsonEncode(response));
+  });
+  router.post('/getGarages', (Request request) async {
+    var json = await request.readAsString();
+    var data = await jsonDecode(json);
+    final response = await getGaragesList(sql);
     return Response.ok(jsonEncode(response));
   });
   router.post('/getMessages', (Request request) async {
--- lib/get_garages.dart
+++ lib/get_garages.dart
@@ -1,5 +1,5 @@
 import 'package:mysql_client/mysql_client.dart';
-Future<List> getUserCarList(String id, MySQLConnection sql) async {
+Future<List> getGaragesList(MySQLConnection sql) async {
   List garages = [];
   final response = await sql.execute(
     "SELECT * FROM users where rules = 1",
