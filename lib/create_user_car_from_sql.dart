a5937a5f5764ebce7304d8b95efe8bd43ff9cdc1
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-03-29 09:18:46 +0300

    ...

--- lib/create_user_car_from_sql.dart
+++ lib/create_user_car_from_sql.dart
@@ -19,7 +19,6 @@ Future<int> createUserCarFromSQL({
   await sql.execute(
       "insert into usercars (id, uid, name, brand, model, year, car_reg) values (${id_int + 1}, $uid, '$name', '$brand', '$model', $year, '$car_reg');");
   return id_int+1;
-  //   "insert into usertable (id, name, password_hash, city, email, country, age, freelancer, last_login, date_of_burn, avatar, skills, education, experience, about_me, client_visiting, servises, rating, reviews, email_succes) values (${id_int + 1}, '$name', '$password_hash', '$city', '$email', '$country', $age, $freelancer, '$last_login', '$date_of_burn', '$avatar', '$skills', '$education', '$experience', '$about_me', '$client_visiting', '$servises', $rating, '$reviews', $email_succes);");
 }
 
 Future<void> updateUserCarFromSQL({
@@ -33,5 +32,4 @@ Future<void> updateUserCarFromSQL({
 }) async {
   await sql.execute(
       "update usercars set name='$name', brand='$brand', model='$model', year=$year, car_reg='$car_reg' where id =$id;");
-  //   "insert into usertable (id, name, password_hash, city, email, country, age, freelancer, last_login, date_of_burn, avatar, skills, education, experience, about_me, client_visiting, servises, rating, reviews, email_succes) values (${id_int + 1}, '$name', '$password_hash', '$city', '$email', '$country', $age, $freelancer, '$last_login', '$date_of_burn', '$avatar', '$skills', '$education', '$experience', '$about_me', '$client_visiting', '$servises', $rating, '$reviews', $email_succes);");
 }
