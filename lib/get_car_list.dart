d4654fe52228d015b907b90dc539461d75fe6eea
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-12 05:28:35 +0300

    ...

--- lib/get_car_list.dart
+++ lib/get_car_list.dart
@@ -26,9 +26,9 @@ import 'package:mysql_client/mysql_client.dart';
     cars.add(
       {
         'like_id': like_id,
-        'cash': data['cash'],
         'liked': like.toString(),
         'id': data['id'],
+        'cash': data['cash'],
         'name': data['name'],
         'brand': data['brand'],
         'model': data['model'],
@@ -79,7 +79,25 @@ print( directory.listSync().length);
     }
     car =
       {
-        'id': data['id'],
+        'cash': data['cash'],
+        'name': data['name'],
+        'brand': data['brand'],
+        'model': data['model'],
+        'price_usd': data['price_usd'],
+        'price_aed': data['price_aed'],
+        'color': data['color'],
+        'killometers': data['killometers'],
+        'regional_specs': data['regional_specs'],
+        'transmission': data['transmission'],
+        'steering_whell': data['steering_whell'],
+        'motor_trim': data['motor_trim'],
+        'body': data['body'],
+        'state': data['state'],
+        'guarantee': data['guarantee'],
+        'service_contact': data['service_contact'],
+        'ccid': data['ccid'],
+        'year': data['year'],
+        'description': data['description'],
         'images': directory.listSync().length,
         'like_id': like_id,
         'liked': like.toString(),
