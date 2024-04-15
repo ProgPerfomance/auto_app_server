33473d1a841198df63a6611b9f2daa6fe94f8a51
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-16 02:05:09 +0300

    ...

--- lib/get_booking_list_from_sql.dart
+++ lib/get_booking_list_from_sql.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: empty_catches
+
 import 'package:mysql_client/mysql_client.dart';
 
 Future<List> getUserBookingList(String id, MySQLConnection sql) async {
@@ -58,56 +60,64 @@ Future<List> getUserBookingListMaster(String id, MySQLConnection sql) async {
     } catch (e) {
       garage_name = '';
     }
-   DateTime.parse(data['timestamp']!).millisecondsSinceEpoch > DateTime.now().subtract(Duration(days: 3)).millisecondsSinceEpoch ?
-    booking.add(
-      {
-        'garage_name': garage_name,
-        'car_name': car.rows.first.assoc()['name'],
-        'description': data['description'],
-        'reason': data['reason'],
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': data['status'],
-      },
-    ) : {booking.add(
-     {
-       'garage_name': garage_name,
-       'car_name': car.rows.first.assoc()['name'],
-       'description': data['description'],
-       'reason': data['reason'],
-       'id': data['id'],
-       'sid': data['sid'],
-       'cid': data['cid'],
-       'uid': data['uid'],
-       'car_brand': car.rows.first.assoc()['brand'],
-       'car_reg': car.rows.first.assoc()['car_reg'],
-       'car_model': car.rows.first.assoc()['model'],
-       'car_year': car.rows.first.assoc()['year'],
-       'owner_name': data['owner_name'],
-       'owner_email': data['owner_email'],
-       'owner_phone': data['owner_phone'],
-       'pickup': data['pickup'],
-       'delivery': data['delivery'],
-       'timestamp': data['timestamp'],
-       'date_time': data['date_time'],
-       'status': 'time is up',
-     },
-   ),
-   await  sql.execute("update booking set status = 'time is up' where id = ${data['id']}"),
-   };
+    try {
+      DateTime
+          .parse(data['timestamp']!)
+          .millisecondsSinceEpoch > DateTime
+          .now()
+          .subtract(Duration(days: 3))
+          .millisecondsSinceEpoch ?
+      booking.add(
+        {
+          'garage_name': garage_name,
+          'car_name': car.rows.first.assoc()['name'],
+          'description': data['description'],
+          'reason': data['reason'],
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': data['status'],
+        },
+      ) : {booking.add(
+        {
+          'garage_name': garage_name,
+          'car_name': car.rows.first.assoc()['name'],
+          'description': data['description'],
+          'reason': data['reason'],
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': 'time is up',
+        },
+      ),
+        await sql.execute(
+            "update booking set status = 'time is up' where id = ${data['id']}"),
+      };
+    } catch (e) {}
   }
   return List.from(booking.reversed);
 }
@@ -132,54 +142,63 @@ Future<List> getNewBookingListMaster(String id, MySQLConnection sql) async {
       "SELECT * FROM usercars where id = ${data['cid']}",
       {},
     );
-    DateTime.parse(data['timestamp']!).millisecondsSinceEpoch > DateTime.now().subtract(Duration(days: 3)).millisecondsSinceEpoch ?
-    booking.add(
-      {
-        'car_name': car.rows.first.assoc()['name'],
-        'description': data['description'],
-        'reason': data['reason'],
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': data['status'],
-      },
-    ) : {booking.add(
-      {
-        'car_name': car.rows.first.assoc()['name'],
-        'description': data['description'],
-        'reason': data['reason'],
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': 'time is up',
-      },
-    ),
-      await  sql.execute("update booking set status = 'time is up' where id = ${data['id']}"),
-    };
+    try {
+      DateTime
+          .parse(data['timestamp']!)
+          .millisecondsSinceEpoch > DateTime
+          .now()
+          .subtract(Duration(days: 3))
+          .millisecondsSinceEpoch ?
+      booking.add(
+        {
+          'car_name': car.rows.first.assoc()['name'],
+          'description': data['description'],
+          'reason': data['reason'],
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': data['status'],
+        },
+      ) : {booking.add(
+        {
+          'car_name': car.rows.first.assoc()['name'],
+          'description': data['description'],
+          'reason': data['reason'],
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': 'time is up',
+        },
+      ),
+        await sql.execute(
+            "update booking set status = 'time is up' where id = ${data['id']}"),
+
+      };
+    }catch (e) {}
   }
   return List.from(booking.reversed);
 }
@@ -211,59 +230,68 @@ Future<List> getManagerBookingList(MySQLConnection sql) async {
     } catch (e) {
       garage_name = '';
     }
-    DateTime.parse(data['timestamp']!).millisecondsSinceEpoch > DateTime.now().subtract(Duration(days: 3)).millisecondsSinceEpoch ?
-    booking.add(
-      {
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'garage': data['garage'],
-        'garage_name': garage_name,
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'car_name': car.rows.first.assoc()['name'],
-        'service_name': service.rows.first.assoc()['name'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': data['status'],
-        'description': data['description'],
-      },
-    ) : {booking.add(
-      {
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'garage': data['garage'],
-        'garage_name': garage_name,
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'car_name': car.rows.first.assoc()['name'],
-        'service_name': service.rows.first.assoc()['name'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': data['status'],
-        'description': data['description'],
-      },
-    ),
-      await  sql.execute("update booking set status = 'time is up' where id = ${data['id']}"),
-    };
-  }
+    try {
+      DateTime
+          .parse(data['timestamp']!)
+          .millisecondsSinceEpoch > DateTime
+          .now()
+          .subtract(Duration(days: 3))
+          .millisecondsSinceEpoch ?
+      booking.add(
+        {
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'garage': data['garage'],
+          'garage_name': garage_name,
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'car_name': car.rows.first.assoc()['name'],
+          'service_name': service.rows.first.assoc()['name'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': data['status'],
+          'description': data['description'],
+        },
+      ) : {booking.add(
+        {
+          'id': data['id'],
+          'sid': data['sid'],
+          'cid': data['cid'],
+          'uid': data['uid'],
+          'garage': data['garage'],
+          'garage_name': garage_name,
+          'car_brand': car.rows.first.assoc()['brand'],
+          'car_reg': car.rows.first.assoc()['car_reg'],
+          'car_model': car.rows.first.assoc()['model'],
+          'car_year': car.rows.first.assoc()['year'],
+          'car_name': car.rows.first.assoc()['name'],
+          'service_name': service.rows.first.assoc()['name'],
+          'owner_name': data['owner_name'],
+          'owner_email': data['owner_email'],
+          'owner_phone': data['owner_phone'],
+          'pickup': data['pickup'],
+          'delivery': data['delivery'],
+          'timestamp': data['timestamp'],
+          'date_time': data['date_time'],
+          'status': data['status'],
+          'description': data['description'],
+        },
+      ),
+        await sql.execute(
+            "update booking set status = 'time is up' where id = ${data['id']}"),
+      };
+    } catch (e) {}
+    }
+
   return List.from(booking.reversed);
 }
 
@@ -294,7 +322,9 @@ Future<List> getManagerNewBookingList(MySQLConnection sql) async {
     } catch (e) {
       garage_name = '';
     }
+    try {
     DateTime.parse(data['timestamp']!).millisecondsSinceEpoch > DateTime.now().subtract(Duration(days: 3)).millisecondsSinceEpoch || data['status'] != 'Pending' ?
+
     booking.add(
       {
         'id': data['id'],
@@ -320,31 +350,35 @@ Future<List> getManagerNewBookingList(MySQLConnection sql) async {
         'description': data['description'],
       },
     ) : {booking.add(
-      {
-        'id': data['id'],
-        'sid': data['sid'],
-        'cid': data['cid'],
-        'uid': data['uid'],
-        'garage': data['garage'],
-        'garage_name': garage_name,
-        'car_brand': car.rows.first.assoc()['brand'],
-        'car_reg': car.rows.first.assoc()['car_reg'],
-        'car_model': car.rows.first.assoc()['model'],
-        'car_year': car.rows.first.assoc()['year'],
-        'car_name': car.rows.first.assoc()['name'],
-        'service_name': service.rows.first.assoc()['name'],
-        'owner_name': data['owner_name'],
-        'owner_email': data['owner_email'],
-        'owner_phone': data['owner_phone'],
-        'pickup': data['pickup'],
-        'delivery': data['delivery'],
-        'timestamp': data['timestamp'],
-        'date_time': data['date_time'],
-        'status': data['status'],
-        'description': data['description'],
-      },
-    ),
-      await  sql.execute("update booking set status = 'time is up' where id = ${data['id']}"),
+  {
+  'id': data['id'],
+  'sid': data['sid'],
+  'cid': data['cid'],
+  'uid': data['uid'],
+  'garage': data['garage'],
+  'garage_name': garage_name,
+  'car_brand': car.rows.first.assoc()['brand'],
+  'car_reg': car.rows.first.assoc()['car_reg'],
+  'car_model': car.rows.first.assoc()['model'],
+  'car_year': car.rows.first.assoc()['year'],
+  'car_name': car.rows.first.assoc()['name'],
+  'service_name': service.rows.first.assoc()['name'],
+  'owner_name': data['owner_name'],
+  'owner_email': data['owner_email'],
+  'owner_phone': data['owner_phone'],
+  'pickup': data['pickup'],
+  'delivery': data['delivery'],
+  'timestamp': data['timestamp'],
+  'date_time': data['date_time'],
+  'status': data['status'],
+  'description': data['description'],
+  },
+  ),
+  await  sql.execute("update booking set status = 'time is up' where id = ${data['id']}"),
+  };
+
+    }  catch (e) {
+
     };
   }
   return List.from(booking.reversed);
