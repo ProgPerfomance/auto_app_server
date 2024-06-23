eff992506cb5b4ab342f27c2908467133aca2632
SmartCherry <jekcatpopov@gmail.com>
2024-06-24 01:09:33 +0300

    ...

---
+++ lib/push_service.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+
+void globalPush(title,body) async {
+  Dio dio = Dio();
+  await dio.post(
+      'https://fcm.googleapis.com/v1/projects/dwd-app-3e56e/messages:send',
+      options: Options(headers: {
+        'Content-Type': 'application/json',
+        'Authorization':
+        "Bearer ya29.a0AXooCguznPfeLv6qCmsfSv7y-LWcThqyDQfIbnfhNQpahYAOs14Fb47EFTHb-Kt8HPUxX2JufSh8aTZmHRvcH7S7Lsf0QEAMj_KCXepYNOTzGkodV482z7fzck2G8z-3XIA8pf6RhzpfPm8tAOKynCB5ac-BQHIE8Lr8aCgYKAWYSARISFQHGX2MiYrXV6JmnrcZUdeXPLJ9rcA0171",
+      }),
+      data: {
+        "message": {
+          "topic": "main",
+          "notification": {
+            "title": title,
+            "body": body,
+          },
+          "data": {
+            "story_id": "story_12345"
+          }
+        }
+      }
+
+  );
+}
